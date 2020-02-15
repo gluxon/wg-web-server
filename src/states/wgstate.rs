8700dc15d17b55874bd7b1d56cedbfb5bfe183a2
Brandon Cheng <brandon.cheng@protonmail.com>
2020-02-15 17:34:51 -0500

    Attempt to create ip device on startup

--- src/states/wgstate.rs
+++ src/states/wgstate.rs
@@ -2,7 +2,7 @@ use crate::config::Config;
 use std::sync::{Mutex, MutexGuard};
 use wireguard_uapi::err::ConnectError;
 use wireguard_uapi::get::Device;
-use wireguard_uapi::{DeviceInterface, WgSocket};
+use wireguard_uapi::{DeviceInterface, RouteSocket, WgSocket};
 
 pub struct WgState {
     // Currently, a call to wireguard_uapi::Socket's get_device() method requires a mutable
@@ -19,6 +19,7 @@ pub struct WgState {
     // I was able to remove the Mutex by patching neli and removing sequence ids, but this was done
     // in a hacky way (and we need sequence ids).
     pub wg_socket: Mutex<WgSocket>,
+    pub route_socket: Mutex<RouteSocket>,
     interface_config: Config,
 }
 
@@ -26,11 +27,19 @@ impl WgState {
     pub fn init(interface_config: Config) -> Result<Self, ConnectError> {
         Ok(Self {
             wg_socket: Mutex::new(WgSocket::connect()?),
+            route_socket: Mutex::new(RouteSocket::connect()?),
             interface_config,
         })
     }
 
     pub fn apply_config(&self) -> Result<(), failure::Error> {
+        let mut route_socket = self.get_route_socket_guard()?;
+        let route_socket = &mut *route_socket;
+
+        // TODO: Handle the result type and continue only if the error is because the device already
+        // exists.
+        let _ = route_socket.add_device(&self.interface_config.name);
+
         let mut wg_guard = self.get_wg_socket_guard()?;
         let wg_socket = &mut *wg_guard;
 
@@ -46,6 +55,18 @@ impl WgState {
             Err(poisoned) => {
                 let mut guard = poisoned.into_inner();
                 *guard = WgSocket::connect()?;
+                Ok(guard)
+            }
+        }
+    }
+
+    fn get_route_socket_guard(&self) -> Result<MutexGuard<RouteSocket>, ConnectError> {
+        match self.route_socket.lock() {
+            Ok(guard) => Ok(guard),
+            // If the mutex for the socket is poisoned, let's just grab a fresh new socket.
+            Err(poisoned) => {
+                let mut guard = poisoned.into_inner();
+                *guard = RouteSocket::connect()?;
                 Ok(guard)
             }
         }
