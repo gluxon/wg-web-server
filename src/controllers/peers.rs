ff47a4a9f949460da82720829682893ad2480e72
Brandon Cheng <brandon.cheng@protonmail.com>
2020-03-03 00:23:02 -0500

    Return 400 when adding a peer failed

--- src/controllers/peers.rs
+++ src/controllers/peers.rs
@@ -3,7 +3,9 @@ use crate::lang;
 use crate::states::WgState;
 use crate::utils::FormInputResult;
 use askama::Template;
+use rocket::http::Status;
 use rocket::request::Form;
+use rocket::response::status;
 use rocket::{get, State};
 use rocket::{post, FromForm};
 use std::borrow::Cow;
@@ -27,7 +29,10 @@ pub struct AddPeer<'v> {
 }
 
 #[post("/add", data = "<form>")]
-pub fn post_add(wg: State<WgState>, form: Form<AddPeer>) -> AddPeerTemplate<'static> {
+pub fn post_add(
+    wg: State<WgState>,
+    form: Form<AddPeer>,
+) -> status::Custom<AddPeerTemplate<'static>> {
     // TODO:
     //   - Record this new peer in the database.
     //   - Calculate the next available IP and give it to this peer.
@@ -37,19 +42,22 @@ pub fn post_add(wg: State<WgState>, form: Form<AddPeer>) -> AddPeerTemplate<'sta
     let public_key = match add_peer.public_key {
         Ok(public_key) => public_key,
         Err(public_key_err) => {
-            return AddPeerTemplate {
+            println!("{}", public_key_err.input);
+            let template = AddPeerTemplate {
                 public_key_err: Some(format!("{}", public_key_err.error)),
                 ..Default::default()
             };
+            return status::Custom(Status::BadRequest, template);
         }
     };
 
     let add_peer_result = wg.add_peer(public_key.as_bytes());
-    AddPeerTemplate {
+    let template = AddPeerTemplate {
         status: Some(match add_peer_result {
             Ok(_) => format!("{} {}", lang::ADD_PEER_SUCCESS, public_key).into(),
             Err(_) => lang::ADD_PEER_ERROR.into(),
         }),
         ..Default::default()
-    }
+    };
+    status::Custom(Status::Ok, template)
 }
